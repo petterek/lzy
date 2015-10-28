dee1078c059a37ace4b9fa8cda4817b8aa4afd4e
PEtter Ekrann <petter@ekrann.no>
2015-10-28 15:13:15 +0100

    Added files

---
+++ Framework/Utils/json/GuidParser.vb
@@ -0,0 +1,19 @@
+Namespace Utils.Json
+    Public Class GuidParser
+        Inherits Builder
+
+        Public sub New
+            MyBase.New(GetType(Guid))
+        End sub
+
+        Public Overrides Function Parse(nextChar As IReader) As Object
+            TokenAcceptors.WhiteSpace(nextChar)
+            TokenAcceptors.Quote(nextChar)
+            TokenAcceptors.BufferLegalCharacters(nextChar, "0123456789ABCDEF-{}")
+            Dim val = New Guid( nextChar.Buffer)
+            TokenAcceptors.Quote(nextChar)
+            Return val 
+
+        End Function
+    End Class
+End NameSpace
