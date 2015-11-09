6582af50da938408807f6420f8c5303c8c8b4658
PEtter Ekrann <petter@ekrann.no>
2015-11-09 10:37:42 +0100

    Commented in some code in the transformer that was commentet out during refactoring

--- LazyFramework.CQRS/Transform/Handling.vb
+++ LazyFramework.CQRS/Transform/Handling.vb
@@ -71,11 +71,12 @@ Namespace Transform
         Public Shared Function TransformAndAddAction(ByVal action As IAmAnAction, ByVal transformer As ITransformEntityToDto, e As Object) As Object
             Dim securityContext As Object
             If transformer Is Nothing Then Return Nothing
-            'If TypeOf (e) Is IProvideSecurityContext Then
-            '    securityContext = DirectCast(e, IProvideSecurityContext).Context
-            'Else
-            '    securityContext = e
-            'End If
+
+            If TypeOf (e) Is IProvideSecurityContext Then
+                securityContext = DirectCast(e, IProvideSecurityContext).Context
+            Else
+                securityContext = e
+            End If
 
             If Not ActionSecurity.Current.EntityIsAvailableForUser(action.ExecutionProfile, action, securityContext) Then Return Nothing
 
