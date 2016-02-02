94491ea9900d3c4e3265060baeb85728de5d3e0f
Espen Kalnes <espen.kalnes@infotjenester.no>
2016-02-02 14:46:07 +0100

    Added ActionListForAction to limit actions that will be checked in the Action list

--- Framework/CQRS/ActionInfo.vb
+++ Framework/CQRS/ActionInfo.vb
@@ -64,7 +64,7 @@ Namespace CQRS
             Return ret
         End Function
 
-        Public Shared Function GetAvailableActionsForEntity(user As IPrincipal, entity As Object) As List(Of IActionBase)
+        Public Shared Function GetAvailableActionsForEntity(user As IPrincipal, entity As Object, ByVal fromAction As IActionBase) As List(Of IActionBase)
             Dim ret As New List(Of IActionBase)
 
             If TypeOf entity Is ActionContext.ActionContext Then
@@ -74,22 +74,57 @@ Namespace CQRS
                     End If
                 Next
             Else
-                If AllActions.ContainsKey(entity.GetType) Then
-                    For Each t In _actionsForType(entity.GetType)
-                        If GetType(CQRS.Query.QueryBase).IsAssignableFrom(t) Then Continue For 'We do not want quries in action list. 
-
-                        Dim createInstance As IActionBase = CType(Activator.CreateInstance(t), IActionBase)
-
-                        If CheckAvailability(entity, createInstance, user) Then
-                            ret.Add(createInstance)
+                If ActionConstraintsForAction.ContainsKey(fromAction.GetType) Then
+                    For Each a In ActionConstraintsForAction(fromAction.GetType).Actions
+                        If CheckAvailability(entity, a, user) Then
+                            ret.Add(a)
                         End If
                     Next
+                Else
+                    If AllActions.ContainsKey(entity.GetType) Then
+                        For Each t In AllActions(entity.GetType)
+
+                            If GetType(CQRS.Query.QueryBase).IsAssignableFrom(t) Then Continue For 'We do not want quries in action list. 
+
+                            Dim createInstance As IActionBase = CType(Activator.CreateInstance(t), IActionBase)
+
+                            If CheckAvailability(entity, createInstance, user) Then
+                                ret.Add(createInstance)
+                            End If
+                        Next
+                    End If
+
                 End If
             End If
 
-
             Return ret
         End Function
+
+        Private Shared _ActionConstraintsForAction As Dictionary(Of Type, IActionListForAction)
+        Private Shared PadLock As New Object
+        Private Shared ReadOnly Property ActionConstraintsForAction As Dictionary(Of Type, IActionListForAction)
+            Get
+                If _ActionConstraintsForAction Is Nothing Then
+                    SyncLock PadLock
+                        If _ActionConstraintsForAction Is Nothing Then
+                            Dim temp As New Dictionary(Of Type, IActionListForAction)
+
+                            For Each el In TypeValidation.FindAllClassesOfTypeInApplication(GetType(IActionListForAction))
+                                If el.IsAbstract Then Continue For
+
+                                Dim getGenericArguments As Type = el.BaseType.GetGenericArguments(0)
+                                If Not temp.ContainsKey(getGenericArguments) Then
+                                    temp.Add(getGenericArguments, CType(Activator.CreateInstance(el), IActionListForAction))
+                                End If
+                            Next
+
+                            _ActionConstraintsForAction = temp
+                        End If
+                    End SyncLock
+                End If
+                Return _ActionConstraintsForAction
+            End Get
+        End Property
 
         Private Shared Function CheckAvailability(ByVal entity As Object, ByVal createInstance As IActionBase, ByVal user As IPrincipal) As Boolean
 
@@ -104,5 +139,14 @@ Namespace CQRS
             End If
             Return False
         End Function
+    End Class
+
+    Public Interface IActionListForAction
+        Function Actions() As IEnumerable(Of IActionBase)
+    End Interface
+    Public MustInherit Class ActionListForAction(Of T As IActionBase)
+        implements IActionListForAction
+
+        Public MustOverride Function Actions() As IEnumerable(Of IActionBase) Implements IActionListForAction.Actions
     End Class
 End Namespace
