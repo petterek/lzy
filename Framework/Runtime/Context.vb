44e8ecef00d5c8c54fe4c5644043537fcb6a5b28
Espen Kalnes <espen.kalnes@infotjenester.no>
2016-03-09 14:19:42 +0100

    Fixed bug when spawning new threads in multiple levels

--- Framework/Runtime/Context.vb
+++ Framework/Runtime/Context.vb
@@ -6,13 +6,14 @@ Namespace Runtime
         Private Shared _current As IContext
         Public Shared Property Current As IContext
             Get
-                SyncLock StoreLock
-                    If CtxStore.ContainsKey(Thread.CurrentThread.ManagedThreadId) Then
-                        Return CtxStore(Thread.CurrentThread.ManagedThreadId)
-                    Else
-                        Return _current
-                    End If
-                End SyncLock
+                If CtxStore.ContainsKey(Thread.CurrentThread.ManagedThreadId) Then
+                    SyncLock StoreLock
+                        If CtxStore.ContainsKey(Thread.CurrentThread.ManagedThreadId) Then
+                            Return CtxStore(Thread.CurrentThread.ManagedThreadId).Peek
+                        End If
+                    End SyncLock
+                End If
+                Return _current
             End Get
             Set(value As IContext)
                 _current = value
@@ -20,13 +21,13 @@ Namespace Runtime
         End Property
 
         Private Shared PadLock As New Object
-        Private Shared _ctxStore As IDictionary(Of Integer, IContext)
-        Private Shared ReadOnly Property CtxStore As IDictionary(Of Integer, IContext)
+        Private Shared _ctxStore As IDictionary(Of Integer, Stack(Of IContext))
+        Private Shared ReadOnly Property CtxStore As IDictionary(Of Integer, Stack(Of IContext))
             Get
                 If _ctxStore Is Nothing Then
                     SyncLock PadLock
                         If _ctxStore Is Nothing Then
-                            _ctxStore = New Dictionary(Of Integer, IContext)
+                            _ctxStore = New Dictionary(Of Integer, Stack(Of IContext))
                         End If
                     End SyncLock
                 End If
@@ -37,7 +38,10 @@ Namespace Runtime
         Private Shared StoreLock As New Object
         Public Shared Sub AddOverrideContext(ctx As IContext)
             SyncLock StoreLock
-                CtxStore(Thread.CurrentThread.ManagedThreadId) = ctx
+                If Not CtxStore.ContainsKey(Thread.CurrentThread.ManagedThreadId) Then
+                    CtxStore(Thread.CurrentThread.ManagedThreadId) = New Stack(Of IContext)
+                End If
+                CtxStore(Thread.CurrentThread.ManagedThreadId).Push(ctx)
             End SyncLock
 
         End Sub
@@ -45,7 +49,10 @@ Namespace Runtime
 
         Public Shared Sub RemoveContext()
             SyncLock StoreLock
-                CtxStore.Remove(Thread.CurrentThread.ManagedThreadId)
+                CtxStore(Thread.CurrentThread.ManagedThreadId).Pop()
+                If not CtxStore(Thread.CurrentThread.ManagedThreadId).Any then
+                    CtxStore.Remove(Thread.CurrentThread.ManagedThreadId)
+                End If
             End SyncLock
         End Sub
 
