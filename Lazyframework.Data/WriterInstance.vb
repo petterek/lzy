a41411dcc7e05650dc7ed592173582e541ba3610
Petter Ekrann <pan@infotjenester.no>
2017-03-17 15:18:18 +0100

    Added the classes back to data. ARgh

---
+++ Lazyframework.Data/WriterInstance.vb
@@ -0,0 +1,34 @@
+﻿
+<Obsolete("Use the one in MSSQLServer instead")> Public Class WriterInstance
+    Implements IWriterInstance
+
+    Private ReadOnly connectionInfo As ServerConnectionInfo
+
+    Public Sub New(connectionInfo As ServerConnectionInfo)
+        If connectionInfo Is Nothing Then
+            Throw New System.ArgumentNullException(NameOf(connectionInfo))
+        End If
+
+        Me.connectionInfo = connectionInfo
+    End Sub
+
+    Public Sub Create(command As CommandInfo) Implements IWriterInstance.Create
+        command.TypeOfCommand = CommandTypeEnum.Create
+        Store.Exec(connectionInfo, command)
+    End Sub
+
+    Public Sub Delete(command As CommandInfo) Implements IWriterInstance.Delete
+        command.TypeOfCommand = CommandTypeEnum.Delete
+        Store.Exec(connectionInfo, command)
+    End Sub
+
+    Public Sub Update(command As CommandInfo) Implements IWriterInstance.Update
+        command.TypeOfCommand = CommandTypeEnum.Update
+        Store.Exec(connectionInfo, command)
+    End Sub
+
+    Public Function Create(Of T)(command As CommandInfo) As T Implements IWriterInstance.Create
+        command.TypeOfCommand = CommandTypeEnum.Create
+        Return CType(Store.ExecScalar(connectionInfo, command), T)
+    End Function
+End Class
